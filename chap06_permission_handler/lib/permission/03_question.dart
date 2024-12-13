4497269f8216ccce02beddc123e1b1e0496ce117
kimsua <k021113a@naver.com>
2024-12-13 16:21:30 +0900

    commit

---
+++ chap06_permission_handler/lib/permission/03_question.dart
@@ -0,0 +1,110 @@
+
+/*
+*  - 카메라로 사진 촬영 시 이미지가 세로로 정렬됨. [미구현]
+*   -> 이미지가 세로로 정렬되려면 컬럼 안에 children 안에 맵 [미구현]
+*  - 이미지가 변수로 저장됨. [미구현]
+*  - 이미지 클릭 시 다이얼 로그로 '미리보기' 텍스트 넣음. [미구현]
+*  - 이미지 미리보기 는 다이얼 로그로 이미지 삭제 유무 지정 [미구현]
+*  - 이미지 촬영 후 등록 시 로딩 아이콘 뜨게 [미구현]
+*   -> bool isLoading = false;
+* */
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraQuestionPage extends StatefulWidget {
+  const CameraQuestionPage({super.key});
+
+  @override
+  State<CameraQuestionPage> createState() => _State();
+}
+
+class _State extends State<CameraQuestionPage> {
+
+  List<File> _images = [];
+  final ImagePicker _picker = ImagePicker();
+  bool _isLoading = false;
+
+  // 갤러리에서 이미지를 선택하는 메소드
+  Future<void> _pickImageFromImage() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final XFile? pickeredFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickeredFile != null) {
+      setState(() {
+        _images.add(File(pickeredFile.path));
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  // 카메라에서 이미지 촬영하는 메소드
+  Future<void> _pickImageFromCamera() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final XFile? pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() {
+        _images.add(File(pickedFile.path));
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+// 이미지를 ㅅ ㅏ제할 때 확인 다이얼 로그
+  void _shwoDeleteDialog(File image) {
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        title: Text("image Delete"),
+        content: Text("정말로 이 이미지를 삭제하겠습니까?"),
+        actions: [
+          TextButton(onPressed: (){
+            setState(() {
+              _images.remove(image);
+            });
+            Navigator.pop(context);
+            Navigator.pop(context);
+          }, child: Text("삭제")),
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: Text("취소"))
+        ],
+      );
+    });
+  }
+
+  // 컬럼 안에 컬럼 버튼 버튼
+  // 센터 안에 컬럼 안에 컬럼,버튼 버튼과 같이 있는 컬럼 안에 images.map
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _isLoading
+            ? CircularProgressIndicator()
+            :
+          SizedBox(height: 20,),
+          // 갤러리에서 선택하는 버튼
+          ElevatedButton(onPressed: _pickImageFromImage, child: Text("갤러리에서 사진 선택")),
+          SizedBox(height: 20,),
+          // 카메라로 사진 찍는 버튼
+          ElevatedButton(onPressed: _pickImageFromCamera, child: Text("카메라로 사진 찍기")),
+          SizedBox(height: 20,)
+        ],
+      ),
+    );
+  }
+}
